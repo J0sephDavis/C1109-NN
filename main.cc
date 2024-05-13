40ef17a9d240b15d18d909373f13d134af7e0fea
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-05-13 12:46:58 -0400

    network object created

--- main.cc
+++ main.cc
@@ -74,50 +74,100 @@ class layer {
 		//overload to allow accessing each neuron using [index]?
 		std::vector<std::unique_ptr<perceptron>> neurons;
 		int width; //neurons in layer
-		int input_width; //inputs to layer
+		int input_width; //inputs to layer. 0 ifeq input layer.
+};
+
+class network {
+public:
+	network(int width, int depth) {
+		this->depth = depth;
+		this->width = width;
+		//generate
+		for (int i = 0; i < depth-1; i++) {
+			layers.emplace_back(layer(width,width, sign));
+		}
+		layers.emplace_back(layer(1, width, passthrough)); //single output node
+	}
+	//compute output of network given input
+	std::vector<int> compute(std::vector<int> input, bool print=true) {
+		if (!print) {
+			std::vector<int> last_output = layers[0].output(input);
+			for (int i = 1; i < depth; i++){
+				last_output = layers[i].output(last_output);
+			}
+			return last_output;
+		}
+		std::cout << "INPUT: ";
+		for (auto x : input)
+			std::cout << x << ",";
+		std::cout << "\n";
+		std::vector<std::vector<int>> outputs;
+		outputs.push_back(layers[0].output(input));
+		for (int i = 1; i < depth; i++) {
+			outputs.push_back(layers[i].output(outputs[i-1]));
+		}
+		//interpret
+		for (int i = 0; i < depth; i++) { //for-each layer
+			std::cout <<  "Layer[" << i << "]->";
+			layer *L = &layers[i];
+			int layer_width = L->width;
+			int input_width = L->input_width;
+			//Output vector
+			for (int k = 0; k < layer_width; k++) { //print output
+				std::cout << outputs[i][k] << ",";
+			}
+			std::cout << "\n";
+			//Neuron weights
+			for (int j = 0; j < layer_width; j++) { //for-each neuron
+				std::cout << "neuron[" << j << "]: w{";
+				for (int k = 0; k < input_width; k++) { //print weights
+					std::cout << L->neurons[j]->weights[k] << ",";
+				}
+				std::cout << "}\n";
+			}
+		}
+		return std::move(outputs[depth]);
+	}
+	void measure(std::vector<int> input, std::vector<int> target) {
+		//process input
+		std::vector<std::vector<int>> outputs
+			= { layers[0].output(input) };
+		for (int i = 1; i < depth; i++) {
+			outputs.emplace_back(layers[i].output(outputs[i-1]));
+		}
+		//display
+		std::cout << "Target: ";
+		for (auto& t : target) std::cout << t << ",";
+		std::cout << "\nIn: ";
+		for (auto& i : input) std::cout << i << ",";
+		for (int j = 0; j < depth; j++) {
+			if (j+1 == depth)
+				std::cout << "\nOut: ";
+			else
+				std::cout << "\n" << j << ": ";
+			for (auto& o : outputs[j]) std::cout << o << ",";
+		}
+		//calc error & display
+		std::cout << "\nERROR = ";
+		for (int x = 0; x < layers[depth-1].width; x++) {
+			std::cout << target[x]-outputs[depth-1][x];
+			std::cout << ",";
+		}
+		std::cout << "\n";
+
+	}
+private:
+	int width,depth;
+	std::vector<layer> layers; //0 = first-layer, last is output; input 'layer' is just the vector given
 };
 
 int main(void) {
 	std::srand(time(NULL));
 	//preparations
 	const int width = 2;
-	const int depth = 2;
-	std::vector<int> input = {0,1}; //total inputs must == width
-	//generate
-	std::vector<layer> layers; //0 = input, last is output
-	for (int i = 0; i < depth-1; i++) {
-		layers.emplace_back(layer(width,width, sign));
-	}
-	layers.emplace_back(layer(1, width, passthrough)); //single output node
-	//output
-	std::vector<std::vector<int>> outputs;
-	outputs.push_back(layers[0].output(input));
-	for (int i = 1; i < depth; i++) {
-		outputs.push_back(layers[i].output(outputs[i-1]));
-	}
-	//interpret
-	std::cout << "INPUT: ";
-	for (auto x : input)
-		std::cout << x << ",";
-	std::cout << "\n";
-	for (int i = 0; i < depth; i++) { //for-each layer
-		std::cout <<  "Layer[" << i << "]->";
-		layer *L = &layers[i];
-		int layer_width = L->width;
-		int input_width = L->input_width;
-		//Output vector
-		for (int k = 0; k < layer_width; k++) { //print output
-			std::cout << outputs[i][k] << ",";
-		}
-		std::cout << "\n";
-		//Neuron weights
-		for (int j = 0; j < layer_width; j++) { //for-each neuron
-			std::cout << "neuron[" << j << "]: w{";
-			for (int k = 0; k < input_width; k++) { //print weights
-				std::cout << L->neurons[j]->weights[k] << ",";
-			}
-			std::cout << "}\n";
-		}
-	}
+	const int depth = 3;
+	network n(width,depth); //the network
+	n.compute({0,1});	 //total inputs must == width
+	n.measure({0,1},{2});
 	return 0;
 }
