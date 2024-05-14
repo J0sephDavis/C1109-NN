2cabee57550c78300d2b82ad2a944d5e03abe47c
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-05-14 18:33:08 -0400

    Changed network values to float values. Backpropagation implementation"

--- main.cc
+++ main.cc
@@ -4,35 +4,38 @@
 #include <vector>
 #include <iostream>
 #include <memory> //unique_ptr
+#include <math.h>
 
 //a single perceptron
 class perceptron {
 	public:
 		perceptron(int count_inputs) {
 			for (int i = 0; i < count_inputs; i++) {
-				weights.push_back((std::rand()% 10) - 4);
+				weights.push_back((std::rand()% 4 + 1));
 			}
 		}
-		int calculate(const std::vector<int> input) {
-			int weighted_sum = 0;
+		float calculate(const std::vector<float> input) {
 			for (size_t i = 0; i < input.size(); i++) {
 				weighted_sum += input[i] * weights[i];
 			}
 			return activation(weighted_sum);
 		};
-		virtual int activation(int input) {
-			//passthrough
-			return input;
+		virtual float activation(float input) {
+			//logistic sigmoid
+			output = 1/(1+exp(-input)); //possible underflow
+			return output;
 		}
-		std::vector<int> weights;
-		//how to store activation function? hardcode within calculate() for now?
+		std::vector<float> weights;
+		float weighted_sum = 0;
+		float error_contribution = 0;
+		float output = 0.0; //the last output of this node.
 };
 class sign_perceptron : public perceptron {
 	public:
 		sign_perceptron(int count_inputs) : perceptron(count_inputs) {
 			//
 		}
-		int activation(int input) override {
+		float activation(float input) override {
 			//sign activation function
 			const int threshold = 0;
 			if (input < threshold) return -1;
@@ -60,12 +63,13 @@ class layer {
 				neurons.emplace_back(new perceptron(input_width));
 			}
 			else if (type == sign) for (int i = 0; i < width; i++) {
-				neurons.emplace_back(new sign_perceptron(input_width));
+				//neurons.emplace_back(new sign_perceptron(input_width));
+				neurons.emplace_back(new perceptron(input_width));
 			}
 			else throw std::runtime_error("INVALID ACTIVATION TYPE");
 		};
-		std::vector<int> output(std::vector<int> input) {
-			std::vector<int> out = {};
+		std::vector<float> output(std::vector<float> input) {
+			std::vector<float> out = {};
 			for (size_t i = 0; i < neurons.size(); i++) {
 				out.push_back(neurons[i]->calculate(input));
 			}
@@ -79,29 +83,23 @@ class layer {
 
 class network {
 public:
-	network(int width, int depth) {
+	network(int input_width, int width, int depth) {
 		this->depth = depth;
 		this->width = width;
 		//generate
-		for (int i = 0; i < depth-1; i++) {
+		layers.emplace_back(layer(width,input_width, sign));
+		for (int i = 1; i < depth-1; i++) {
 			layers.emplace_back(layer(width,width, sign));
 		}
 		layers.emplace_back(layer(1, width, passthrough)); //single output node
 	}
 	//compute output of network given input
-	std::vector<int> compute(std::vector<int> input, bool print=true) {
-		if (!print) {
-			std::vector<int> last_output = layers[0].output(input);
-			for (int i = 1; i < depth; i++){
-				last_output = layers[i].output(last_output);
-			}
-			return last_output;
-		}
+	std::vector<std::vector<float>> compute(std::vector<float> input) {
 		std::cout << "INPUT: ";
 		for (auto x : input)
 			std::cout << x << ",";
 		std::cout << "\n";
-		std::vector<std::vector<int>> outputs;
+		std::vector<std::vector<float>> outputs;
 		outputs.push_back(layers[0].output(input));
 		for (int i = 1; i < depth; i++) {
 			outputs.push_back(layers[i].output(outputs[i-1]));
@@ -126,47 +124,95 @@ public:
 				std::cout << "}\n";
 			}
 		}
-		return std::move(outputs[depth]);
+		return std::move(outputs);
 	}
-	void measure(std::vector<int> input, std::vector<int> target) {
-		//process input
-		std::vector<std::vector<int>> outputs
-			= { layers[0].output(input) };
-		for (int i = 1; i < depth; i++) {
-			outputs.emplace_back(layers[i].output(outputs[i-1]));
-		}
-		//display
-		std::cout << "Target:\t";
-		for (auto& t : target) std::cout << t << ",";
-		std::cout << "\nIn:\t";
-		for (auto& i : input) std::cout << i << ",";
-		for (int j = 0; j < depth; j++) {
-			if (j+1 == depth)
-				std::cout << "\nOut:\t";
-			else
-				std::cout << "\n" << j << ":\t";
-			for (auto& o : outputs[j]) std::cout << o << ",";
-		}
-		//calc error & display
-		std::cout << "\nERROR = ";
-		for (int x = 0; x < layers[depth-1].width; x++) {
-			std::cout << target[x]-outputs[depth-1][x] << ",";
-		}
-		std::cout << "\n";
-
-	}
-private:
 	int width,depth;
 	std::vector<layer> layers; //0 = first-layer, last is output; input 'layer' is just the vector given
 };
 
 int main(void) {
-	std::srand(time(NULL));
 	//preparations
-	const int width = 2;
+	std::srand(time(NULL));
+	const int width = 6;
 	const int depth = 3;
-	network n(width,depth); //the network
-	n.compute({0,1});	 //total inputs must == width
-	n.measure({0,1},{2});
+	const std::vector<float> inputs = {1,1};
+	const std::vector<float> expected_output = {1.0};
+	const float learning_rate = 0.25;
+	network n(inputs.size(), width,depth); //the network
+	std::vector<std::vector<float>> output = n.compute(inputs);
+
+	std::cout << "compute gradient\n";
+	//get error contribution of output nodes
+	for (size_t i = 0; i < output.back().size(); i++){
+		std::cout << "==\n";
+		auto& neuron = n.layers.back().neurons.at(i);
+		float error_signal = expected_output.at(i) - neuron->output;
+		float derivative = (neuron->output * (1-neuron->output));
+		float error_contribution = error_signal * derivative;
+		std::cout << "output error/contrib: " << error_signal << "/"
+			<< error_contribution << "\n";
+		//d_pj = (t_pj - o_pj) * o_pi * (1-o_pj)
+		neuron->error_contribution = error_contribution;
+		//DELTA RULE
+		for (size_t weight_index = 0;
+				weight_index < neuron->weights.size()-1;
+				weight_index++) {
+			std::cout << "w_" << weight_index << "\t"
+				<< neuron->weights.at(weight_index) << "\n";
+			auto& input_neuron = n.layers.at(n.layers.size()-2).neurons.at(weight_index);
+			float delta_rule = -learning_rate * error_contribution * input_neuron->output;
+			std::cout << "delta W\t"
+				<< delta_rule << "\n";
+			neuron->weights.at(weight_index) = neuron->weights.at(weight_index) + delta_rule;
+
+		}
+	}
+	for (int i = n.layers.size()-2; i >= 0; i--) {
+		std::cout << "==\nLayer-" << i << "\n";
+		auto& layer = n.layers.at(i);
+		auto& upper_layer = n.layers.at(i+1);
+		std::vector<float> input_values = {};
+		if (i == 0) {
+			input_values = inputs;
+		}
+		else {
+			for (size_t a = 0; a < n.layers.at(i-1).neurons.size()-1; a++) {
+				auto& tmp = n.layers.at(i-1).neurons.at(a);
+				input_values.push_back(tmp->output);
+			}
+		}
+		std::cout << "layer input:";
+		for (auto& inval : input_values) {
+			std::cout << inval << ",";
+		}
+		std::cout <<"\n";
+		for (size_t j = 0; j < layer.neurons.size(); j++) {
+			//d_pk = f_j'(z_pj) * SUM_k (d_pk * w_kj)
+			//where the k-neurons are an upper layer
+			std::cout << ">Neuron-" << j << "\n";
+			auto& neuron = layer.neurons.at(j);
+			float derivative = neuron->output * (1 - neuron->output);
+			float associated_error = 0;
+			for (size_t k = 0; k < upper_layer.neurons.size(); k++) { //nodes this feeds into
+				auto& uppper_neuron = upper_layer.neurons.at(k);
+				//d_pk * w_kj
+				float upper_contribution = uppper_neuron->error_contribution
+					* uppper_neuron->weights.at(j);
+				associated_error += upper_contribution;
+			}
+			std::cout << "sum_k:\t" << associated_error << "\n";
+			neuron->error_contribution = associated_error * derivative; //d_pk
+			std::cout << "d_pk:\t" << neuron->error_contribution << "\n";
+			std::cout << "WEIGHT COUNT:" << neuron->weights.size() << "\n";
+			for (size_t l = 0; l <= neuron->weights.size()-1;l++) {
+
+				float input_value = input_values[l];
+				float delta_rule = -learning_rate * neuron->error_contribution *
+					input_value;
+				std::cout << "delta(w_" << l << "):\t" << delta_rule << "\n";
+				neuron->weights[l] = delta_rule + neuron->weights[l];
+			}
+		}
+	}
 	return 0;
 }
