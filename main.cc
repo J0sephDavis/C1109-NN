91909b0e33101053ef9359fcdcdf66598fa3cc37
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-05-17 12:18:43 -0400

    error contribution and weight updates are now done at separate stages.

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
-#define PRINT_TRAINING
+//#define PRINT_TRAINING
+#define PRINT_ERRCON
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -38,23 +39,24 @@ class perceptron {
 			for (auto& w : weights) {
 				std::cout << w << ",";
 			}
-			std::cout << "\n";
 		}
 		//given an array of inputs, determines the weight changes needed
 		virtual void train(float learning_rate, std::vector<float> input) {
 			//DELTA RULE - the change in weight (from neuron u_i to u_j) is
-			//  equal to -1 * learning rate, multiplied by
+			//  equal to learning rate, multiplied by
 			//  error contribution (d_pj) then multiplied by the output of
 			//  neuron-i (u_i)
 			for (size_t weight_index = 0;
-					weight_index < weights.size()-1;
+					weight_index < weights.size();
 					weight_index++) {
-				float delta_weight = -learning_rate *
+				float delta_weight = learning_rate *
 					error_contribution *
 					input[weight_index];
 				weights.at(weight_index) += delta_weight;
 #ifdef PRINT_TRAINING
-				std::cout << "dw:" << delta_weight << ", ";
+				std::cout << "dw-" << weight_index
+					<< ":" << delta_weight << ", ";
+				std::cout << "\n";
 #endif
 			}
 		}
@@ -66,25 +68,45 @@ class perceptron {
 };
 class bias_perceptron : public perceptron {
 public:
-	bias_perceptron() : perceptron(1) {
-		//
+	bias_perceptron() : perceptron(0) {
+		output = 1;
+		derivative = 0;
 	}
 	float calculate(const std::vector<float> input) override {
 		(void)input;
-		output = 1;
 		return output;
 	}
 	void revealWeights() override {
-		std::cout << "bias.\n";
+		std::cout << "bias=" << output << "\n";
+	}
+	void train(float learning_rate, std::vector<float> input) override {
+		(void) input;
+		//TODO revisit
+		output = output - learning_rate*error_contribution;
+		return;
 	}
 };
 class pass_perceptron : public perceptron {
 public:
-	pass_perceptron(size_t input_width) : perceptron(input_width, false) {
+	pass_perceptron() : perceptron(1, true) {
 		derivative = 1; //TODO prove
 	}
 	float calculate(const std::vector<float> input) override {
 		return input[0]; //passthrough. This will take special consideration when passing input
+	}
+	void train(float learning_rate, std::vector<float> input) override {
+		//DELTA RULE - the change in weight (from neuron u_i to u_j) is
+		//  equal to learning rate, multiplied by
+		//  error contribution (d_pj) then multiplied by the output of
+		//  neuron-i (u_i)
+		float delta_weight = learning_rate * error_contribution *
+			input[0];
+		weights.at(0) += delta_weight;
+#ifdef PINING
+		std::cout << "dw-" << weight_index
+			<< ":" << delta_weight << ", ";
+		std::cout << "\n";
+#endif
 	}
 };
 enum perceptron_type {
@@ -100,12 +122,11 @@ public:
 	 * input_width - number of neurons in previous layer.
 	 * 	The accepted number of inputs for this layer.
 	 */
-	layer(size_t width, size_t input_width, size_t bias_neurons,
+	layer(size_t _width, size_t _input_width, size_t _bias_neurons,
 			perceptron_type type = logistic) {
-		this->width = width+bias_neurons;
-		this->input_width = input_width;
-		this->bias_neurons = bias_neurons;
-		std::cout << "Layer_iw:\t" << input_width << "\n";
+		this->width = _width+_bias_neurons;
+		this->input_width = _input_width;
+		this->bias_neurons = _bias_neurons;
 		size_t neuron_index = 0;
 		for (; neuron_index < bias_neurons; neuron_index++) {
 			neurons.emplace_back(new bias_perceptron());
@@ -116,48 +137,68 @@ public:
 		}
 		else if (type == passthrough) for (; neuron_index < width;
 				neuron_index++) {
-			neurons.emplace_back(new pass_perceptron(input_width));
+			neurons.emplace_back(new pass_perceptron());
 		}
 		else throw std::runtime_error("INVALID ACTIVATION TYPE");
 	};
 	std::vector<float> output(std::vector<float> input) {
 		std::vector<float> out = {};
-		for (size_t i = 0; i < neurons.size(); i++) {
-			out.push_back(neurons[i]->calculate(input));
+		size_t neuron_index = 0;
+		for (; neuron_index < bias_neurons; neuron_index++)
+			out.push_back(neurons[neuron_index]->calculate({}));
+		for (; neuron_index < neurons.size(); neuron_index++) {
+			out.push_back(neurons[neuron_index]->calculate(input));
 		}
 		return std::move(out);
 	}
-	/*layer::train()
+	/* update_err_contrib
 	 * Computes error contribution/strength for each neuron in the layer,
-	 * due to the layer they feed into.
-	 * Calls the perceptrons training function. */
-	virtual void train(std::vector<float> input, std::vector<float> label,
-			const float learning_rate,
+	 * */
+	virtual void update_err_contrib(std::vector<float> label,
 			std::shared_ptr<layer> upper_layer) {
-		(void)label; //unused variable warning
-
+		(void)label;
+		//for-each neuron in the current layer
 		for (size_t u_j = 0; u_j < neurons.size(); u_j++) {
 			//d_pj = f_j'(z_pj) * SUM_k (d_pk * w_kj)
 			auto& neuron = neurons.at(u_j);
 		//==Caclulate 2nd half of error equation - SUM_k (d_pk * w_kj==
 			float associated_error = 0;
-			for (size_t k = 0; k < upper_layer->neurons.size(); k++) {
-				auto& uppper_neuron = upper_layer->neurons.at(k+bias_neurons);
+#ifdef PRINT_ERRCON
+			std::cout << "### Neuron " << u_j
+				<< "|k=" << upper_layer->bias_neurons << "\n";
+			std::cout << "sum_k(d_kjw_kj)\t[";
+#endif
+			for (size_t k = upper_layer->bias_neurons;
+					//Assumes the first neurons are always bias neurons.
+					k < upper_layer->neurons.size(); k++) {
+				auto& uppper_neuron = upper_layer->neurons.at(k);
 				//d_pk * w_kj
-				associated_error += uppper_neuron
+				auto error = uppper_neuron
 					->error_contribution * uppper_neuron
 					->weights.at(u_j);
+				associated_error += error;
+#ifdef PRINT_ERRCON
+				std::cout << associated_error << ", ";
+#endif
 			}
 			neuron->error_contribution = associated_error
 				* neuron->derivative; //d_pk
-#ifdef PRINT_TRAINING
-			std::cout << "N-" << u_j << " d: "
-				<< neuron->error_contribution << ", w: "; 
+#ifdef PRINT_ERRCON
+			std::cout << "]= " << associated_error << "\n";
+			std::cout << "derivative\t|" << neuron->derivative << "\n";
+			std::cout << "E contribution\t|"
+				<< neuron->error_contribution << "\n";
+			std::cout << "w\t[";
 			neuron->revealWeights();
+			std::cout << "]\n";
 #endif
-
-			neuron->train(learning_rate, (input));
 		}
+	}
+	/*layer::train()
+	 * Calls the perceptrons training function of each neuron. */
+	virtual void train(std::vector<float> input, const float learning_rate) {
+		for (auto& neuron : neurons)
+			neuron->train(learning_rate, (input));
 	}
 	std::vector<std::unique_ptr<perceptron>> neurons;
 	size_t width; //neurons in layer
@@ -172,94 +213,68 @@ public:
 		: layer(width,input_width,0,type){
 		//
 	}
-	void train(std::vector<float> input, std::vector<float> label,
-			const float learning_rate,
-			std::shared_ptr<layer> upper_layer) override {
-		(void)upper_layer;
-		//get error contribution of output nodes
-		int label_index = 0;
-		for (auto& neuron : neurons) {
-			float error_strength
-				=label.at(label_index++) - neuron->output;
-			neuron->error_contribution = error_strength * neuron->derivative;
-
-#ifdef PRINT_TRAINING
-			std::cout << "N-" << label_index-1 << " d: "
-				<< neuron->error_contribution << ", w: "; 
-			neuron->revealWeights();
-#endif
-			//d_pj = (t_pj - o_pj) * f'(z_pj)
+	void update_err_contrib(std::vector<float> label,
+				std::shared_ptr<layer> upper_layer) override {
+		(void) upper_layer;
+		for (size_t j = 0; j < neurons.size(); j++) {
+			const auto& neuron = neurons.at(j);
 			//t_pj - target output
 			//o_pj - actual output (in this case of the output node)
-			//z_pj - weighted sum of inputs
-			//derivative of logistic = f(x)(1-f(x))
-			neuron->train(learning_rate, std::move(input));
+			float err_str = label.at(j) - neuron->output;
+			//d_pj = (t_pj - o_pj) * f'(z_pj)
+			neuron->error_contribution = err_str * neuron->derivative;
+#ifdef PRINT_ERRCON
+			std::cout << "### Neuron " << j
+				<< " d: " << neuron->error_contribution
+				<< "\nerr_str: " << err_str
+				<< ", w: "; 
+			neuron->revealWeights();
+			std::cout << "\n";
+#endif
+		}
+	}
+	void train(std::vector<float> input, const float learning_rate)override {
+		//get error contribution of output nodes
+		for (size_t j = 0; j < neurons.size(); j++) {
+			const auto& neuron = neurons.at(j);
+			neuron->train(learning_rate, (input));
 		}
 	}
 };
 class input_layer : public layer {
 public:
-	input_layer(size_t input_width, perceptron_type = passthrough) :
-		layer(input_width, input_width, 1) {
+	input_layer(size_t input_width, perceptron_type type = passthrough) :
+		layer(input_width, input_width, 0, type) {
 		//
 	};
 	std::vector<float> output(std::vector<float> input) {
 		std::vector<float> out = {};
 		for (size_t i = 0; i < neurons.size(); i++) {
-			out.push_back(neurons[i]->calculate(input));
+			out.push_back(neurons[i]->calculate({input[i]}));
 		}
 		return std::move(out);
 	}
-	/*layer::train()
-	 * Computes error contribution/strength for each neuron in the layer,
-	 * due to the layer they feed into.
-	 * Calls the perceptrons training function. */
-	virtual void train(std::vector<float> input, std::vector<float> label,
-			const float learning_rate,
-			std::shared_ptr<layer> upper_layer) {
-		(void)label; //unused variable warning
-
-		for (size_t u_j = 0; u_j < neurons.size(); u_j++) {
-			//d_pj = f_j'(z_pj) * SUM_k (d_pk * w_kj)
-			auto& neuron = neurons.at(u_j);
-		//==Caclulate 2nd half of error equation - SUM_k (d_pk * w_kj==
-			float associated_error = 0;
-			for (size_t k = 0; k < upper_layer->neurons.size(); k++) {
-				auto& uppper_neuron = upper_layer->neurons.at(k+bias_neurons);
-				//d_pk * w_kj
-				associated_error += uppper_neuron
-					->error_contribution * uppper_neuron
-					->weights.at(u_j);
-			}
-			neuron->error_contribution = associated_error
-				* neuron->derivative; //d_pk
-#ifdef PRINT_TRAINING
-			std::cout << "N-" << u_j << " d: "
-				<< neuron->error_contribution << ", w: "; 
-			neuron->revealWeights();
-#endif
-
-			neuron->train(learning_rate, (input));
-		}
-	}
 };
 class network {
+#define BIAS_NEURONS 1
 public:
 	network(int input_width, int _width, int _depth) {
 		this->depth = _depth;
 		this->width = _width;
 		//generate
-		layers.emplace_back(new layer(width,input_width, logistic));
+		layers.emplace_back(new input_layer(input_width, passthrough));
 		for (int i = 1; i < depth-1; i++) {
 //layer(size_t width, size_t input_width, perceptron_type type = logistic)
-			layers.emplace_back(new layer(width, layers[i-1]->width,						logistic));
+			layers.emplace_back(new layer(width, layers[i-1]->width,
+						BIAS_NEURONS,logistic));
 		}
 		layers.emplace_back(new output_layer(1, layers[depth-2]->width, logistic)); //single output node
 	}
 	//compute output of network given input
 	std::vector<std::vector<float>> compute(std::vector<float> input) {
 		std::vector<std::vector<float>> outputs;
-		outputs.push_back(layers[0]->output(input));
+		std::shared_ptr<input_layer> inpl = std::dynamic_pointer_cast<input_layer>(layers[0]);
+		outputs.push_back(inpl->output(input));
 		for (int i = 1; i < depth; i++) {
 			outputs.push_back(layers[i]->output(outputs[i-1]));
 		}
@@ -267,45 +282,78 @@ public:
 	}
 	void revealWeights() {
 		for (int lid = layers.size()-1; lid >= 0; lid--) {
-			std::cout << "\nLayer " << lid << ":\n========\nWeights\t|";
+			std::cout << "\nLayer " << lid
+				<< ": (inputs:"	<< layers[lid]->input_width
+				<< ", width" << layers[lid]->width
+				<< ", bias " << layers[lid]->bias_neurons << ")";
+			std::cout << "\n========\nWeights\t|";
 			for (size_t a = 0; a < layers.at(lid)->input_width; a++)
 				std::cout << a << "\t";
 			for (int nid = layers.at(lid)->neurons.size()-1;
 					nid >= 0; nid--) {
 				std::cout << "\nNode " << nid << "\t|";
-				for (auto& w : layers.at(lid)->neurons.at(nid)->weights)
-					std::cout << w << "\t";
+				layers.at(lid)->neurons.at(nid)->revealWeights();
 			}
 			std::cout << "\n";
 		}
 	}
 	void train(float learning_rate, std::vector<float> test, std::vector<float> label) {
+		(void)label;
 		//PREP
 		std::vector<std::vector<float>> output = compute(test);
+#ifdef PRINT_TRAINING
+		std::cout << "\n# BEGIN\n";
+		std::cout << "T:\t[";
+		for (auto& i : test) std::cout << i << ", ";
+		std::cout << "]\n";
+		std::cout << "L:\t[";
+		for (auto& i : label) std::cout << i << ", ";
+		std::cout << "]\n";
+		std::cout << "A:\t[";
+		for(auto& i : output.back()) std::cout << i << ", ";
+		std::cout << "]\n";
+		std::cout << "FULL OUTPUT [\n";
+		for (int layer_out = output.size()-1; layer_out >= 0; layer_out--) {
+			std::cout << "\t[";
+			for (auto& e : output.at(layer_out)) {
+				std::cout << e << ", ";
+			}
+			std::cout << "]\n";
+
+		}
+		std::cout << "]\n";
+#endif
+		std::vector<float> input_values = {};
 		//Backpropagate
 		for (int layer_index = layers.size()-1; layer_index >= 0;
 				layer_index--) {
-#ifdef PRINT_TRAINING
-			std::cout << "# Layer-" << layer_index << "\n";
-#endif
 			//prepare layer input
-			std::vector<float> input_values = {};
 			if (layer_index == 0) 		//INPUT layer
 				input_values = test;
-			else for (size_t input_k = 0; input_k < layers
-					.at(layer_index-1)->neurons.size()-1;
-					input_k++) {
-				auto& tmp = layers.at(layer_index-1)
-					->neurons.at(input_k);
-				input_values.emplace_back(tmp->output);
+			else {
+				input_values = output.at(layer_index-1);
 			}
 			//prepare upper layer
 			std::shared_ptr<layer> upper_layer = NULL;
 			if (layer_index+1 != (int)layers.size())
 				upper_layer = layers.at(layer_index+1);
+#ifdef PRINT_TRAINING
+			std::cout << "\n## Layer " << layer_index << "\n";
+			std::cout << "I:\t[";
+			for (auto& i : input_values) std::cout << i << ", ";
+			std::cout << "]\n";
+			if (upper_layer != NULL)
+				std::cout << "out_width:\t|" << upper_layer->width;
+			std::cout << "\n";
+#endif
 			//TRAIN
-			layers.at(layer_index)->train(input_values, label, 
-				learning_rate, upper_layer);
+			layers.at(layer_index)->update_err_contrib(label, upper_layer);
+		}
+		for (int layer_index = layers.size()-1; layer_index >= 0;
+				layer_index--) {
+			if (layer_index == 0) input_values = test;
+			else input_values = output.at(layer_index-1);
+			layers.at(layer_index)->train(input_values, learning_rate);
 		}
 	}
 	int width,depth;
@@ -317,7 +365,7 @@ int main(void) {
 	//preparations
 	std::srand(time(NULL));
 	const int width = 2;
-	const int depth = 2;
+	const int depth = 4;
 	network n(2, width,depth); //the network
 	//show weights before
 	n.revealWeights();
@@ -339,22 +387,13 @@ int main(void) {
 			<< (expectations[i][0]-actual)<< ")\n";
 
 	}
-	std::cout << "train?";
-	getchar();
-#define EPOCH 64
-#define MILLENIUM 4
-	for (size_t cent = 0; cent < MILLENIUM; cent++) {
-		for (size_t year = 0; year < EPOCH; year++) {
-			for (size_t idx = 0; idx < tests.size(); idx++) {
-				n.train(0.5,tests[idx], expectations[idx]);
-			}
+#define CYCLES 1
+	for (size_t cycle = 0; cycle < CYCLES; cycle++) {
+		for (size_t idx = 0; idx < tests.size(); idx++) {
+			n.train(0.9,tests[idx], expectations[idx]);
 		}
-//		std::cout << "c:" << cent;
-//		n.revealWeights();
-//		getchar();
 	}
-	std::cout << "after";
-	n.revealWeights();
+	std::cout << "trained\n";
 	for (size_t i = 0; i < tests.size(); i++){
 		std::cout << "T:[";
 		for (auto& v : tests[i]) std::cout << v << ", ";
@@ -365,5 +404,7 @@ int main(void) {
 			<< (expectations[i][0]-actual)<< ")\n";
 
 	}
+	getchar();
+	n.revealWeights();
 	return 0;
 }
