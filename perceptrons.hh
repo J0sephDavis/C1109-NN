6b9040a9e33ff92d014ffd8ccf00388c6b6409aa
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-06-16 11:18:17 -0400

    create hyperparameter struct. Update comments & access modifiers. Move delta weight formula to its own function

--- perceptrons.hh
+++ perceptrons.hh
@@ -6,25 +6,51 @@ enum perceptron_type {
 	passthrough,
 	hyperbolic_tangent
 };
+typedef struct hyperparams {
+	hyperparams(const float learning_rate, const float momentum) {
+		this->learning_rate = learning_rate;
+		this->momentum = momentum;
+	}
+	float momentum = 0.0f;
+	float learning_rate = 0.0f;
+} hyperparams;
+
+//the weight change formula to be used during training
+static inline float calc_dw(const hyperparams& params, float err_contrib,
+	//DELTA RULE - the change in weight (from neuron u_i to u_j) is
+	//  equal to learning rate, multiplied by
+	//  error contribution (d_pj) then multiplied by the output of
+	//  neuron-i (u_i)
+		float input, float previous_dw) {
+	return (params.learning_rate * err_contrib * input)
+		+ (params.momentum * previous_dw);
+}
 //
 class perceptron {
 	public:
 		perceptron(int,bool rand_weights = true);
-		//compute sum_i (w_ij * o_ij)
-		float calculate(const std::vector<float> input);
-		virtual float activation(float net_input);
-		virtual void train(const float momentum,
-				const float learning_rate,
-				std::vector<float> input);
-		std::string type_str();
-	public:
-		std::vector<float> weights;
-		float error_contribution = 0;
-		float output = 0.0; //the last output of this node.
-		float derivative = 0;
 		perceptron_type type = UNDEFINED;
-	protected:
-		std::vector<float> delta_weights; //changes in weights
+		//returns the activation functions name. (for logging)
+		const std::string type_str() const;
+	//forward pass
+		//compute sum_i (w_ij * o_ij) - called during a forward pass
+		float calculate(const std::vector<float> input);
+		//the activation function
+		virtual float activation(float net_input);
+		std::vector<float> weights;
+	//training
+		/*train() is virtual for the bias function which changes
+		 * its output value, not input weights*/
+		//called during a backward pass
+		virtual void train(const hyperparams& params, std::vector<float> input);
+		//the error contribution calculated during backdiff
+		float error_contribution = 0.0f;
+		//the output of the node during the forward pass
+		float output = 0.0f;
+		//the derivative of the output during the forward pass: do/dz
+		float derivative = 0.0f;
+		//changes in weights during the previous training session
+		std::vector<float> delta_weights;
 };
 
 class perceptron_htan : public perceptron {
@@ -37,15 +63,14 @@ class bias_perceptron : public perceptron {
 public:
 	bias_perceptron();
 	float activation(float net_input) override;
-	void train(const float momentum, const float learning_rate,
-			std::vector<float> input) override;
+	void train(const hyperparams& params, std::vector<float> input) override;
 private:
 	float delta_output = 0.0f; // change in output during training(n-1)
 };
+
 class pass_perceptron : public perceptron {
 public:
 	pass_perceptron(int net_input_width, std::vector<bool> weight_mask = {});
 	float activation(float net_input) override;
-	void train(const float momentum, float learning_rate,
-			std::vector<float> input) override;
+	void train(const hyperparams& params, std::vector<float> input) override;
 };
