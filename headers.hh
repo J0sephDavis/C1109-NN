e2459a66912144d987f684fec369b123b7653170
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-06-18 09:49:47 -0400

    step in dissolving headers.hh

--- headers.hh
+++ headers.hh
@@ -2,17 +2,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <stdexcept>
-#include <vector>
 #include <iostream>
-#include <memory> //unique_ptr
 #include <math.h>
 #include <fstream>
 #include <iomanip>
 #include <sstream>
-#include <string>
 
 #define MAX_ERAS 10000
 #define EPOCHS 1
 #define THRESHOLD 0.2f
 #define SEED_VAL 9350
-#define BIAS_NEURONS 1
