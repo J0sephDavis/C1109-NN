a76a8d3014c3bf98f721057843028d84ed5e3e80
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-06-19 10:46:12 -0400

    benchmarking will average all tests for output labels

--- network.cc
+++ network.cc
@@ -16,10 +16,19 @@ network::network(const hyperparams params, perceptron_type neuron_t,
 	 //single output node
 	layers.emplace_back(new output_layer(df.label_len, layers[depth-2]->width, neuron_t));
 	//get some testing data
+	std::cout << "testing data{\n";
 	for (size_t i = 0; i < df.data.size() * 0.2; i++) {
 		auto random_index = std::rand() % df.data.size();
 		testingData.push_back(df.data.at(random_index));
+		for (const auto& v : df.data.at(random_index)->data) {
+				std::cout << v << "\t";
+		}
+		for (const auto& v : df.data.at(random_index)->label) {
+				std::cout << v << "\t";
+		}
+		std::cout << "\n";
 	}
+	std::cout << "\n}\n";
 	//train on whole dataset for now
 	for (const auto& sp_inst : df.data) {
 		trainingData.push_back(sp_inst);
@@ -70,15 +79,25 @@ void network::train_on_instance(size_t instance_id) {
  * the length of the test label. Each index will refer to each classification
  * so that the model can be properly graded*/
 std::vector<float> network::benchmark() {
-	std::vector<float> results = {};
+	const size_t output_len = testingData.at(0)->label.size();
+	std::vector<std::vector<float>> classifier_results(output_len);
 	for (const auto& test : testingData) {
 		std::vector<float> actual = std::move(compute(test->data).back());
 		//TODO warn if actual.size() != test.label.size()
 		float average = 0.0f;
-		for (size_t idx = 0 ; idx < actual.size(); idx++)
-			average += std::abs(test->label.at(idx) - actual.at(idx));
-		average /= actual.size();
-		results.emplace_back(std::move(average));
+		for (size_t idx = 0 ; idx < actual.size(); idx++) {
+			classifier_results.at(idx).push_back(
+					std::abs(test->label.at(idx) - actual.at(idx)));
+		}
+	}
+	std::vector<float> results;
+	for (const auto& cResults : classifier_results) {
+		float average = 0.0f;
+		for (size_t i = 0; i < cResults.size(); i++) {
+			average += cResults.at(i);
+		}
+		average /= cResults.size();
+		results.push_back(std::move(average));
 	}
 	return std::move(results);
 }
